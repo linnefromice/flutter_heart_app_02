866b3191e03240ee446d21efd180d26b4c5a00b2
linnefromice <linnefromice@gmail.com>
2021-01-04 00:41:18 +0900

    create new screen for login page (& add common bg)

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class _WrapperCommonBg extends StatelessWidget {
+  _WrapperCommonBg({
+    Key key,
+    this.child
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.red[200],
+              Colors.orange[100],
+              Colors.deepOrange[200]
+            ],
+          )
+      ),
+      child: child,
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _WrapperCommonBg(
+        child: Center(
+          child: Text("Login Page"),
+        ),
+      ),
+    );
+  }
+
+}
