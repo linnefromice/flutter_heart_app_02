2b0e9f8fb1d200f0d5e88a6f9e898bc4f63e2c86
linnefromice <linnefromice@gmail.com>
2021-01-06 11:24:10 +0900

    apply commmon bg to login_page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,40 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:linnefromice/components/wrapper_common_background.dart';
 import 'package:linnefromice/screens/home_page.dart';
-
-class _WrapperCommonBg extends StatelessWidget {
-  _WrapperCommonBg({
-    Key key,
-    this.child
-  }) : super(key: key);
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Colors.red[200],
-              Colors.orange[100],
-              Colors.deepOrange[200]
-            ],
-          )
-      ),
-      child: child,
-    );
-  }
-}
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _WrapperCommonBg(
+      body: WrapperCommonBackground(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
