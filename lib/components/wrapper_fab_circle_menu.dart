77db5ca55f2d9c9192dd73f230e706aa8145505c
linnefromice <linnefromice@gmail.com>
2021-01-09 23:48:41 +0900

    refactor - separate global menu

---
+++ lib/components/wrapper_fab_circle_menu.dart
@@ -0,0 +1,58 @@
+import 'package:fab_circular_menu/fab_circular_menu.dart';
+import 'package:flutter/material.dart';
+
+class WrapperFabCircularMenu extends StatelessWidget {
+  WrapperFabCircularMenu({
+    Key key,
+    this.fabKey,
+  }) : super(key: key);
+
+  final GlobalKey<FabCircularMenuState> fabKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return FabCircularMenu(
+      key: fabKey,
+      alignment: Alignment.bottomCenter,
+      ringColor: Colors.red.withOpacity(0.5),
+      fabColor: Colors.red.withOpacity(0.5),
+      fabOpenIcon: Icon(Icons.menu, color: Colors.white),
+      fabCloseIcon: Icon(Icons.close, color: Colors.white),
+      children: <Widget>[
+        RawMaterialButton(
+          onPressed: () => print("You pressed 1"),
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(24.0),
+          child: Icon(Icons.home, color: Colors.white),
+        ),
+        RawMaterialButton(
+          onPressed: () => print("You pressed 2"),
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(24.0),
+          child: Icon(Icons.supervisor_account, color: Colors.white),
+        ),
+        RawMaterialButton(
+          onPressed: () => print("You pressed 3"),
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(24.0),
+          child: Icon(Icons.save, color: Colors.white),
+        ),
+        RawMaterialButton(
+          onPressed: () => print("You pressed 4. This one closes the menu on tap"),
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(24.0),
+          child: Icon(Icons.settings, color: Colors.white),
+        ),
+        RawMaterialButton(
+          onPressed: () {
+            print("You pressed 5. This one closes the menu on tap");
+            fabKey.currentState.close();
+          },
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(24.0),
+          child: Icon(Icons.close, color: Colors.white),
+        )
+      ],
+    );
+  }
+}
