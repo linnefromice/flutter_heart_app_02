b15abd0a61b1babb4cd3d932f7f7e95e81711089
linnefromice <linnefromice@gmail.com>
2021-01-09 23:01:19 +0900

    update ui (pattern of list)

--- lib/screens/users_page.dart
+++ lib/screens/users_page.dart
@@ -20,19 +20,88 @@ final List datas = [
     "rating": 2,
   },
   {
+    "name": "Amarant Coral",
+    "rating": 4,
+  },
+  {
+    "name": "Freya Crescent",
+    "rating": 3,
+  },
+  {
+    "name": "Quina Quen",
+    "rating": 3,
+  },
+  {
+    "name": "Eiko Carol",
+    "rating": 5,
+  },
+  {
     "name": "Kuja",
     "rating": 5,
   },
+  {
+    "name": "Queen Brahne",
+    "rating": 4,
+  },
+  {
+    "name": "Zorn and Thorn",
+    "rating": 5,
+  },
+  {
+    "name": "Garland",
+    "rating": 3,
+  },
+  {
+    "name": "Necron",
+    "rating": 3,
+  },
 ];
+
+class _UserListTile extends StatelessWidget {
+  _UserListTile({
+    Key key,
+    this.idx,
+    this.name,
+    this.rating,
+  }) : super(key: key);
+  final int idx;
+  final String name;
+  final int rating;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(
+          name.characters.first,
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.grey,
+      ),
+      title: Text(
+        name,
+        style: TextStyle(color: Colors.white),
+      ),
+      subtitle: Row(
+        children: [
+          Icon(Icons.favorite, size: 30, color: rating >= 1 ? Colors.pink.withOpacity(0.5) : Colors.white),
+          Icon(Icons.favorite, size: 30, color: rating >= 2 ? Colors.pink.withOpacity(0.5) : Colors.white),
+          Icon(Icons.favorite, size: 30, color: rating >= 3 ? Colors.pink.withOpacity(0.5) : Colors.white),
+          Icon(Icons.favorite, size: 30, color: rating >= 4 ? Colors.pink.withOpacity(0.5) : Colors.white),
+          Icon(Icons.favorite, size: 30, color: rating >= 5 ? Colors.pink.withOpacity(0.5) : Colors.white),
+        ],
+      ),
+      trailing: RaisedButton(
+        child: Text("Friend"),
+      ) ,
+    );
+  }
+}
 
 class UsersPage extends HookWidget {
   Widget _buildHeader() {
     return Column(
       children: [
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text("Search..."),
-        ),
         Container(
           margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
           child: TextField(
@@ -66,7 +135,11 @@ class UsersPage extends HookWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
-                  children: List.generate(100, (index) => Text("${(index+1).toString()} ${datas[index % 5]["name"]}"))
+                  children: List.generate(100, (index) => _UserListTile(
+                    idx: index,
+                    name: datas[index % 5]["name"],
+                    rating: datas[index % 5]["rating"],
+                  ))
                 ),
               ),
             ),
