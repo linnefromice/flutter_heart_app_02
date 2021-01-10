8efd2f4263107193b4915a5b442a3e5056ccb9bf
linnefromice <linnefromice@gmail.com>
2021-01-11 01:42:31 +0900

    add initializing code for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:linnefromice/screens/users_page.dart';
 
 import 'screens/login_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
 
 class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +19,28 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return Scaffold(body: Center(child: Text("Occurred Error when initialize Firebase ...")));
+          }
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return UsersPage();
+          }
+          // Otherwise, show something whilst waiting for initialization to complete
+          return Scaffold(body: Center(child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              Text("Loading ...")
+            ],
+          )));
+        },
+      ),
     );
   }
 }
