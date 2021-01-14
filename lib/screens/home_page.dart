db41716b7830f5e16bf36ce83b7d833803b78501
linnefromice <linnefromice@gmail.com>
2021-01-14 23:15:08 +0900

    refactor for using firestore

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -31,8 +31,23 @@ final List datas = [
   },
 ];
 
-class HomePage extends HookWidget {
+class HomePage extends StatelessWidget {
   final GlobalKey<FabCircularMenuState> fabKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WrapperCommonBackground(
+        child: _Content(),
+      ),
+      floatingActionButton: Builder(
+        builder: (context) => WrapperFabCircularMenu(fabKey: fabKey)
+      ),
+    );
+  }
+}
+
+class _Content extends HookWidget {
   final int _initialPageIndex = 0;
 
   Widget _buildPageWidget({final BuildContext context, final String name, final String avatarUrl}) {
@@ -72,105 +87,98 @@ class HomePage extends HookWidget {
     final _name = useState(datas[_initialPageIndex]["name"]);
     final _rating = useState(datas[_initialPageIndex]["rating"]);
 
-    return Scaffold(
-      body: WrapperCommonBackground(
-        child: Stack(
-          children: [
-            PageView(
-              controller: _pageController,
-              children: List.generate(datas.length, (index) {
-                final user = datas[index];
-                return _buildPageWidget(
-                  context: context,
-                  name: user["name"],
-                  avatarUrl: user["avatarUrl"]
-                );
-              }),
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.55,
-              left: 0,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ButtonTheme(
-                    minWidth: 50,
-                    height: 50,
-                    child: RaisedButton(
-                      child: Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                      ),
-                      color: Colors.transparent,
-                      shape: CircleBorder(),
-                      onPressed: () {
-                        final int nextPageIndex = _pageController.page.toInt() - 1;
-                        _pageController.animateToPage(
-                          nextPageIndex,
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.easeInOut,
-                        );
-                        _name.value = datas[nextPageIndex]["name"];
-                        _rating.value = datas[nextPageIndex]["rating"];
-                      }
-                    ),
-                  ),
-                  SizedBox(width: 200),
-                  ButtonTheme(
-                    minWidth: 50,
-                    height: 50,
-                    child: RaisedButton(
-                      child: Icon(Icons.arrow_forward, color: Colors.white,),
-                      color: Colors.transparent,
-                      shape: CircleBorder(),
-                      onPressed: () {
-                        final int nextPageIndex = _pageController.page.toInt() + 1;
-                        _pageController.animateToPage(
-                          nextPageIndex,
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.easeInOut,
-                        );
-                        _name.value = datas[nextPageIndex]["name"];
-                        _rating.value = datas[nextPageIndex]["rating"];
-                      }
-                    ),
-                  )
-                ],
-              )
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.65,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: Text(_name.value, style: TextStyle(color: Colors.red)),
-              )
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.70,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(Icons.favorite, size: 50, color: _rating.value >= 1 ? Colors.pink.withOpacity(0.5) : Colors.white),
-                    Icon(Icons.favorite, size: 50, color: _rating.value >= 2 ? Colors.pink.withOpacity(0.5) : Colors.white),
-                    Icon(Icons.favorite, size: 50, color: _rating.value >= 3 ? Colors.pink.withOpacity(0.5) : Colors.white),
-                    Icon(Icons.favorite, size: 50, color: _rating.value >= 4 ? Colors.pink.withOpacity(0.5) : Colors.white),
-                    Icon(Icons.favorite, size: 50, color: _rating.value >= 5 ? Colors.pink.withOpacity(0.5) : Colors.white),
-                  ],
-                )
-              )
-            )
-          ],
+    return Stack(
+      children: [
+        PageView(
+          controller: _pageController,
+          children: List.generate(datas.length, (index) {
+            final user = datas[index];
+            return _buildPageWidget(
+              context: context,
+              name: user["name"],
+              avatarUrl: user["avatarUrl"]
+            );
+          }),
         ),
-      ),
-      floatingActionButton: Builder(
-        builder: (context) => WrapperFabCircularMenu(fabKey: fabKey)
-      ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.55,
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ButtonTheme(
+                minWidth: 50,
+                height: 50,
+                child: RaisedButton(
+                  child: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                  color: Colors.transparent,
+                  shape: CircleBorder(),
+                  onPressed: () {
+                    final int nextPageIndex = _pageController.page.toInt() - 1;
+                    _pageController.animateToPage(
+                      nextPageIndex,
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                    _name.value = datas[nextPageIndex]["name"];
+                    _rating.value = datas[nextPageIndex]["rating"];
+                  }
+                ),
+              ),
+              SizedBox(width: 200),
+              ButtonTheme(
+                minWidth: 50,
+                height: 50,
+                child: RaisedButton(
+                  child: Icon(Icons.arrow_forward, color: Colors.white,),
+                  color: Colors.transparent,
+                  shape: CircleBorder(),
+                  onPressed: () {
+                    final int nextPageIndex = _pageController.page.toInt() + 1;
+                    _pageController.animateToPage(
+                      nextPageIndex,
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                    _name.value = datas[nextPageIndex]["name"];
+                    _rating.value = datas[nextPageIndex]["rating"];
+                  }
+                ),
+              )
+            ],
+          )
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.65,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Text(_name.value, style: TextStyle(color: Colors.red)),
+          )
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.70,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.favorite, size: 50, color: _rating.value >= 1 ? Colors.pink.withOpacity(0.5) : Colors.white),
+                Icon(Icons.favorite, size: 50, color: _rating.value >= 2 ? Colors.pink.withOpacity(0.5) : Colors.white),
+                Icon(Icons.favorite, size: 50, color: _rating.value >= 3 ? Colors.pink.withOpacity(0.5) : Colors.white),
+                Icon(Icons.favorite, size: 50, color: _rating.value >= 4 ? Colors.pink.withOpacity(0.5) : Colors.white),
+                Icon(Icons.favorite, size: 50, color: _rating.value >= 5 ? Colors.pink.withOpacity(0.5) : Colors.white),
+              ],
+            )
+          )
+        )
+      ],
     );
   }
 }
