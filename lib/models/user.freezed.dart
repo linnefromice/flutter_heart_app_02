cc4afeb6f9ca89e1194cbccd40559dc8fce0dde4
linnefromice <linnefromice@gmail.com>
2021-01-16 23:12:15 +0900

    create user model

---
+++ lib/models/user.freezed.dart
@@ -0,0 +1,180 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'user.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$UserTearOff {
+  const _$UserTearOff();
+
+// ignore: unused_element
+  _User call({String name, int rating, bool isFriend, String avatarUrl}) {
+    return _User(
+      name: name,
+      rating: rating,
+      isFriend: isFriend,
+      avatarUrl: avatarUrl,
+    );
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $User = _$UserTearOff();
+
+/// @nodoc
+mixin _$User {
+  String get name;
+  int get rating;
+  bool get isFriend;
+  String get avatarUrl;
+
+  @JsonKey(ignore: true)
+  $UserCopyWith<User> get copyWith;
+}
+
+/// @nodoc
+abstract class $UserCopyWith<$Res> {
+  factory $UserCopyWith(User value, $Res Function(User) then) =
+      _$UserCopyWithImpl<$Res>;
+  $Res call({String name, int rating, bool isFriend, String avatarUrl});
+}
+
+/// @nodoc
+class _$UserCopyWithImpl<$Res> implements $UserCopyWith<$Res> {
+  _$UserCopyWithImpl(this._value, this._then);
+
+  final User _value;
+  // ignore: unused_field
+  final $Res Function(User) _then;
+
+  @override
+  $Res call({
+    Object name = freezed,
+    Object rating = freezed,
+    Object isFriend = freezed,
+    Object avatarUrl = freezed,
+  }) {
+    return _then(_value.copyWith(
+      name: name == freezed ? _value.name : name as String,
+      rating: rating == freezed ? _value.rating : rating as int,
+      isFriend: isFriend == freezed ? _value.isFriend : isFriend as bool,
+      avatarUrl: avatarUrl == freezed ? _value.avatarUrl : avatarUrl as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$UserCopyWith<$Res> implements $UserCopyWith<$Res> {
+  factory _$UserCopyWith(_User value, $Res Function(_User) then) =
+      __$UserCopyWithImpl<$Res>;
+  @override
+  $Res call({String name, int rating, bool isFriend, String avatarUrl});
+}
+
+/// @nodoc
+class __$UserCopyWithImpl<$Res> extends _$UserCopyWithImpl<$Res>
+    implements _$UserCopyWith<$Res> {
+  __$UserCopyWithImpl(_User _value, $Res Function(_User) _then)
+      : super(_value, (v) => _then(v as _User));
+
+  @override
+  _User get _value => super._value as _User;
+
+  @override
+  $Res call({
+    Object name = freezed,
+    Object rating = freezed,
+    Object isFriend = freezed,
+    Object avatarUrl = freezed,
+  }) {
+    return _then(_User(
+      name: name == freezed ? _value.name : name as String,
+      rating: rating == freezed ? _value.rating : rating as int,
+      isFriend: isFriend == freezed ? _value.isFriend : isFriend as bool,
+      avatarUrl: avatarUrl == freezed ? _value.avatarUrl : avatarUrl as String,
+    ));
+  }
+}
+
+/// @nodoc
+class _$_User with DiagnosticableTreeMixin implements _User {
+  const _$_User({this.name, this.rating, this.isFriend, this.avatarUrl});
+
+  @override
+  final String name;
+  @override
+  final int rating;
+  @override
+  final bool isFriend;
+  @override
+  final String avatarUrl;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'User(name: $name, rating: $rating, isFriend: $isFriend, avatarUrl: $avatarUrl)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'User'))
+      ..add(DiagnosticsProperty('name', name))
+      ..add(DiagnosticsProperty('rating', rating))
+      ..add(DiagnosticsProperty('isFriend', isFriend))
+      ..add(DiagnosticsProperty('avatarUrl', avatarUrl));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _User &&
+            (identical(other.name, name) ||
+                const DeepCollectionEquality().equals(other.name, name)) &&
+            (identical(other.rating, rating) ||
+                const DeepCollectionEquality().equals(other.rating, rating)) &&
+            (identical(other.isFriend, isFriend) ||
+                const DeepCollectionEquality()
+                    .equals(other.isFriend, isFriend)) &&
+            (identical(other.avatarUrl, avatarUrl) ||
+                const DeepCollectionEquality()
+                    .equals(other.avatarUrl, avatarUrl)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(name) ^
+      const DeepCollectionEquality().hash(rating) ^
+      const DeepCollectionEquality().hash(isFriend) ^
+      const DeepCollectionEquality().hash(avatarUrl);
+
+  @JsonKey(ignore: true)
+  @override
+  _$UserCopyWith<_User> get copyWith =>
+      __$UserCopyWithImpl<_User>(this, _$identity);
+}
+
+abstract class _User implements User {
+  const factory _User(
+      {String name, int rating, bool isFriend, String avatarUrl}) = _$_User;
+
+  @override
+  String get name;
+  @override
+  int get rating;
+  @override
+  bool get isFriend;
+  @override
+  String get avatarUrl;
+  @override
+  @JsonKey(ignore: true)
+  _$UserCopyWith<_User> get copyWith;
+}
