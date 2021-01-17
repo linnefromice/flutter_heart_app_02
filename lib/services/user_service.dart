8fbb7a61649e76abcf84322e14df00e75526cabb
linnefromice <linnefromice@gmail.com>
2021-01-17 22:09:34 +0900

    refactor service class

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -4,24 +4,30 @@ import 'package:linnefromice/models/user.dart';
 class UserService {
   final _instance = FirebaseFirestore.instance;
 
+  User _generateUserFromQueryDocumentSnapshot(QueryDocumentSnapshot queryDocumentSnapshot) => User(
+    name: queryDocumentSnapshot.data()["name"],
+    rating: queryDocumentSnapshot.data()["rating"],
+    isFriend: queryDocumentSnapshot.data()["isFriend"],
+    avatarUrl: queryDocumentSnapshot.data()["avatarUrl"],
+  );
+
   Stream<List<User>> streamUsers() {
     return _instance.collection("users").snapshots().map(
-      (QuerySnapshot querySnapshot) => querySnapshot.docs.map((QueryDocumentSnapshot queryDocumentSnapshot) => User(
-        name: queryDocumentSnapshot.data()["name"],
-        rating: queryDocumentSnapshot.data()["rating"],
-        isFriend: queryDocumentSnapshot.data()["isFriend"],
-        avatarUrl: queryDocumentSnapshot.data()["avatarUrl"],
-      )).toList()
+      (QuerySnapshot querySnapshot) => querySnapshot.docs.map(
+        (QueryDocumentSnapshot snapshot) => _generateUserFromQueryDocumentSnapshot(snapshot)
+      ).toList()
     );
   }
 
+  User _generateUserFromDocumentSnapshot(DocumentSnapshot snapshot) => User(
+    name: snapshot.data()["name"],
+    rating: snapshot.data()["rating"],
+    isFriend: snapshot.data()["isFriend"],
+    avatarUrl: snapshot.data()["avatarUrl"],
+  );
+
   Future<List<User>> findUsers() async {
     QuerySnapshot querySnapshot = await _instance.collection("users").get();
-    return querySnapshot.docs.map((DocumentSnapshot document) => User(
-      name: document.data()["name"],
-      rating: document.data()["rating"],
-      isFriend: document.data()["isFriend"],
-      avatarUrl: document.data()["avatarUrl"],
-    )).toList();
+    return querySnapshot.docs.map((DocumentSnapshot document) => _generateUserFromDocumentSnapshot(document)).toList();
   }
 }
