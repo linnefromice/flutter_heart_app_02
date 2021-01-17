641dfdb4669bbbf80d9b625bca782f2023cdc95e
linnefromice <linnefromice@gmail.com>
2021-01-17 22:16:28 +0900

    refactor

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -3,18 +3,19 @@ import 'package:linnefromice/models/user.dart';
 
 class UserService {
   final _instance = FirebaseFirestore.instance;
+  final _collectionName = "users";
 
-  User _generateUserFromQueryDocumentSnapshot(QueryDocumentSnapshot queryDocumentSnapshot) => User(
-    name: queryDocumentSnapshot.data()["name"],
-    rating: queryDocumentSnapshot.data()["rating"],
-    isFriend: queryDocumentSnapshot.data()["isFriend"],
-    avatarUrl: queryDocumentSnapshot.data()["avatarUrl"],
+  User _generateUserFromQueryDocumentSnapshot(QueryDocumentSnapshot snapshot) => User(
+    name: snapshot.data()["name"],
+    rating: snapshot.data()["rating"],
+    isFriend: snapshot.data()["isFriend"],
+    avatarUrl: snapshot.data()["avatarUrl"],
   );
 
   Stream<List<User>> streamUsers() {
-    return _instance.collection("users").snapshots().map(
+    return _instance.collection(_collectionName).snapshots().map(
       (QuerySnapshot querySnapshot) => querySnapshot.docs.map(
-        (QueryDocumentSnapshot snapshot) => _generateUserFromQueryDocumentSnapshot(snapshot)
+        (QueryDocumentSnapshot queryDocumentSnapshot) => _generateUserFromQueryDocumentSnapshot(queryDocumentSnapshot)
       ).toList()
     );
   }
@@ -27,7 +28,9 @@ class UserService {
   );
 
   Future<List<User>> findUsers() async {
-    QuerySnapshot querySnapshot = await _instance.collection("users").get();
-    return querySnapshot.docs.map((DocumentSnapshot document) => _generateUserFromDocumentSnapshot(document)).toList();
+    QuerySnapshot querySnapshot = await _instance.collection(_collectionName).get();
+    return querySnapshot.docs.map(
+      (DocumentSnapshot documentSnapshot) => _generateUserFromDocumentSnapshot(documentSnapshot)
+    ).toList();
   }
 }
