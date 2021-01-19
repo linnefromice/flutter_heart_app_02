529463ef9a5169171fb0267e0c206b448f1799e1
linnefromice <linnefromice@gmail.com>
2021-01-19 23:55:10 +0900

    update login page - add navigation to debug page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:linnefromice/components/wrapper_common_background.dart';
+import 'package:linnefromice/screens/add_user_page.dart';
 import 'package:linnefromice/screens/home_page.dart';
 
 final List<String> domainList = [
@@ -224,6 +225,35 @@ class LoginPage extends HookWidget {
                   domain: _selectedDomain.value,
                   password: _passwordController.text
                 ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton.icon(
+                icon: Icon(
+                  Icons.build,
+                  size: 24,
+                  color: Colors.white,
+                ),
+                label: Text(
+                  "DEBUG\nadd user",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  onPrimary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                ),
+                onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => AddUserPage()
+                  )
+                )
               ),
             )
           ],
