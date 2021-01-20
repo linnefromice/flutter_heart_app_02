c52ebb1acc721219b8c53d1851efa604b74301d0
linnefromice <linnefromice@gmail.com>
2021-01-20 23:01:57 +0900

    create form (only design)

--- lib/screens/add_user_page.dart
+++ lib/screens/add_user_page.dart
@@ -6,9 +6,52 @@ class AddUserPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperCommonBackground(
-        child: Center(
-          child: Text("AddUserPage"),
-        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0),
+              child: Text("Add User"),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0),
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  hintText: "Name"
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0),
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "rating"
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0),
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: "avatarUrl"
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0),
+              child: RaisedButton(
+                child: Text("SUBMIT"),
+              ),
+            )
+          ],
+        )
       ),
     );
   }
