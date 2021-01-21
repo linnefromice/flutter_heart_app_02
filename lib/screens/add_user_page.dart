0753fba95e6734910f6471f8c31912425b73c58e
linnefromice <linnefromice@gmail.com>
2021-01-22 03:00:48 +0900

    refactor

--- lib/screens/add_user_page.dart
+++ lib/screens/add_user_page.dart
@@ -21,60 +21,78 @@ class AddUserPage extends HookWidget {
           children: [
             Container(
               margin: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text("Add User"),
+              child: _buildPageTitle(),
             ),
             Container(
               margin: EdgeInsets.symmetric(vertical: 8.0),
               padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: TextField(
-                controller: _nameController,
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(
-                  hintText: "Name"
-                ),
-              ),
+              child: _buildNameField(_nameController),
             ),
             Container(
               margin: EdgeInsets.symmetric(vertical: 8.0),
               padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: TextField(
-                controller: _ratingController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: "rating"
-                ),
-              ),
+              child: _buildRatingField(_ratingController),
             ),
             Container(
               margin: EdgeInsets.symmetric(vertical: 8.0),
               padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: TextField(
-                controller: _avatarUrlController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  hintText: "avatarUrl"
-                ),
-              ),
+              child: _buildAvatarUrlField(_avatarUrlController),
             ),
             Container(
               margin: EdgeInsets.symmetric(vertical: 8.0),
-              child: RaisedButton(
-                child: Text("SUBMIT"),
-                onPressed: () {
-                  userService.createUser(
-                    User(
-                      name: _nameController.text,
-                      rating: int.parse(_ratingController.text),
-                      isFriend: false, // initial status
-                      avatarUrl: _avatarUrlController.text,
-                    )
-                  );
-                },
-              ),
+              child: _buildSubmitButton(_nameController, _ratingController, _avatarUrlController),
             )
           ],
         )
       ),
+    );
+  }
+  
+  Text _buildPageTitle() => Text("Add User");
+  
+  TextField _buildNameField(TextEditingController _nameController) {
+    return TextField(
+      controller: _nameController,
+      keyboardType: TextInputType.name,
+      decoration: InputDecoration(
+        hintText: "Name"
+      ),
+    );
+  }
+  
+  TextField _buildRatingField(TextEditingController _ratingController) {
+    return TextField(
+      controller: _ratingController,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          hintText: "rating"
+      ),
+    );
+  }
+  
+  TextField _buildAvatarUrlField(TextEditingController _avatarUrlController) {
+    return TextField(
+      controller: _avatarUrlController,
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+          hintText: "avatarUrl"
+      ),
+    );
+  }
+
+  RaisedButton _buildSubmitButton(TextEditingController _nameController, TextEditingController _ratingController, TextEditingController _avatarUrlController) {
+    return RaisedButton(
+      child: Text("SUBMIT"),
+      onPressed: () {
+        userService.createUser(
+            User(
+              name: _nameController.text,
+              rating: int.parse(_ratingController.text),
+              isFriend: false, // initial status
+              avatarUrl: _avatarUrlController.text,
+            )
+        );
+      },
     );
   }
 }
