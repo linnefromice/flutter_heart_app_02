d2ac00ffb3a1534083ae7bd02ff3cd66786f283e
linnefromice <linnefromice@gmail.com>
2021-01-22 03:25:12 +0900

    use clip board

--- lib/screens/add_user_page.dart
+++ lib/screens/add_user_page.dart
@@ -1,3 +1,4 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:linnefromice/components/wrapper_common_background.dart';
@@ -96,7 +97,9 @@ class AddUserPage extends HookWidget {
         ),
         IconButton(
           icon: Icon(Icons.crop_free),
-          onPressed: () => {},
+          onPressed: () => FlutterClipboard.paste().then((value) {
+            _avatarUrlController.text = value;
+          }),
         ),
       ],
     );
