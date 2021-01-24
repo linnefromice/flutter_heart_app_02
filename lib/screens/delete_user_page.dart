c72f82b7398ad19a2f5e438d0bb0828ad090230a
linnefromice <linnefromice@gmail.com>
2021-01-24 11:02:07 +0900

    create new screen

---
+++ lib/screens/delete_user_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:linnefromice/components/wrapper_common_background.dart';
+
+class DeleteUserPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WrapperCommonBackground(
+        child: Center(
+          child: Text("Delete User Page"),
+        ),
+      ),
+    );
+  }
+}
