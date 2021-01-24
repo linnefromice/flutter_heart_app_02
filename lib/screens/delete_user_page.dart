a1ea33b03551fee8522ef1d1825a3abe64010adf
linnefromice <linnefromice@gmail.com>
2021-01-24 11:30:33 +0900

    add displaying datas in delete page

--- lib/screens/delete_user_page.dart
+++ lib/screens/delete_user_page.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:linnefromice/components/wrapper_common_background.dart';
+import 'package:linnefromice/models/user.dart';
+import 'package:linnefromice/services/user_service.dart';
 
 class DeleteUserPage extends StatelessWidget {
+  final userService = UserService();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperCommonBackground(
-        child: Center(
-          child: Text("Delete User Page"),
-        ),
+        child: FutureBuilder<List<User>>(
+          future: userService.findUsers(),
+          builder: (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            }
+            if (snapshot.data == null) {
+              return Center(child: Text("NO DATA"));
+            }
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                final item = snapshot.data[index];
+                return Card(
+                  child: ListTile(
+                    leading: Icon(Icons.close),
+                    title: Text(item.name),
+                    trailing: Text(item.rating.toString()),
+                  ),
+                );
+              },
+            );
+          },
+        )
       ),
     );
   }
