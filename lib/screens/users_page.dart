1175eb94828bb4e2e8d84d0d04d996d46d9cd4db
linnefromice <linnefromice@gmail.com>
2021-01-26 01:12:28 +0900

    apply to users_page

--- lib/screens/users_page.dart
+++ lib/screens/users_page.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:connectivity/connectivity.dart';
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:linnefromice/components/rated_heart.dart';
 import 'package:linnefromice/components/wrapper_common_background.dart';
 import 'package:linnefromice/components/wrapper_fab_circle_menu.dart';
 import 'package:linnefromice/models/user.dart';
@@ -121,11 +124,11 @@ class _UserListTile extends StatelessWidget {
       ),
       subtitle: Row(
         children: [
-          Icon(Icons.favorite, size: 30, color: rating >= 1 ? Colors.pink.withOpacity(0.5) : Colors.white),
-          Icon(Icons.favorite, size: 30, color: rating >= 2 ? Colors.pink.withOpacity(0.5) : Colors.white),
-          Icon(Icons.favorite, size: 30, color: rating >= 3 ? Colors.pink.withOpacity(0.5) : Colors.white),
-          Icon(Icons.favorite, size: 30, color: rating >= 4 ? Colors.pink.withOpacity(0.5) : Colors.white),
-          Icon(Icons.favorite, size: 30, color: rating >= 5 ? Colors.pink.withOpacity(0.5) : Colors.white),
+          RatedHeart(rate: min(1, max(0, rating.toDouble() - 0)), size: 30), // origin -> Icon(Icons.favorite, size: 30, color: rating >= 1 ? Colors.pink.withOpacity(0.5) : Colors.white)
+          RatedHeart(rate: min(1, max(0, rating.toDouble() - 1)), size: 30),
+          RatedHeart(rate: min(1, max(0, rating.toDouble() - 2)), size: 30),
+          RatedHeart(rate: min(1, max(0, rating.toDouble() - 3)), size: 30),
+          RatedHeart(rate: min(1, max(0, rating.toDouble() - 4)), size: 30),
         ],
       ),
       trailing: isFriend ? RaisedButton(
