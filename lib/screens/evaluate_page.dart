906b5093740fd3b36a519ea69575e2217c8478f2
linnefromice <linnefromice@gmail.com>
2021-01-27 10:08:16 +0900

    update design

--- lib/screens/evaluate_page.dart
+++ lib/screens/evaluate_page.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:linnefromice/components/avatar_area.dart';
+import 'package:linnefromice/components/rated_heart.dart';
 import 'package:linnefromice/components/wrapper_common_background.dart';
 import 'package:linnefromice/models/user.dart';
 
@@ -10,21 +14,70 @@ class EvaluatePage extends StatelessWidget {
 
   final User user;
 
+  AvatarArea _buildAvatarArea({final double diameter}) {
+    return AvatarArea(
+      diameter: diameter,
+      image: !(user.avatarUrl == null || user.avatarUrl == "") ? NetworkImage(user.avatarUrl) : null,
+      child: user.avatarUrl == null || user.avatarUrl == "" ? Text("NO IMAGE", style: TextStyle(color: Colors.black)) : null,
+    );
+  }
+
+  Text _buildNameArea() => Text(
+    user.name,
+    textAlign: TextAlign.center,
+    style: TextStyle(
+        fontWeight: FontWeight.w400,
+        fontSize: 30.0,
+        color: Colors.white
+    ),
+  );
+
+  Row _buildRatingArea() => Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      RatedHeart(rate: min(1, max(0, user.rating - 0)), size: 50), // origin -> Icon(Icons.favorite, size: 50, color: user.rating >= 1 ? Colors.pink.withOpacity(0.5) : Colors.white),
+      RatedHeart(rate: min(1, max(0, user.rating - 1)), size: 50),
+      RatedHeart(rate: min(1, max(0, user.rating - 2)), size: 50),
+      RatedHeart(rate: min(1, max(0, user.rating - 3)), size: 50),
+      RatedHeart(rate: min(1, max(0, user.rating - 4)), size: 50),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperCommonBackground(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
-            Text("id: ${user.id}"),
-            Text("name: ${user.name}"),
-            Text("rating: ${user.rating.toString()}"),
-            Text("avatarUrl: ${user.avatarUrl}"),
-            Text("isFriend: ${user.isFriend.toString()}"),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.25,
+              left: 0,
+              right: 0,
+              child: _buildAvatarArea(
+                  diameter: MediaQuery.of(context).size.width * 0.8
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.65,
+              left: 0,
+              right: 0,
+              child: _buildNameArea(),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.70,
+              left: MediaQuery.of(context).size.width * 0.1,
+              right: MediaQuery.of(context).size.width * 0.1,
+              child: Divider(height: 40, thickness: 2, color: Colors.white),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.75,
+              left: 0,
+              right: 0,
+              child: _buildRatingArea(),
+            )
           ],
-        ),
+        )
       ),
     );
   }
