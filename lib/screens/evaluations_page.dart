082b5180ffe8a511d830775537105ac5ccb230eb
linnefromice <linnefromice@gmail.com>
2021-01-29 10:05:21 +0900

    create screen

---
+++ lib/screens/evaluations_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:linnefromice/components/wrapper_common_background.dart';
+
+class EvaluationsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WrapperCommonBackground(
+        child: Center(child: Text("EvaluationPage")),
+      ),
+    );
+  }
+}
