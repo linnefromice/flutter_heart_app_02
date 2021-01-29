0e79b6e204ad6911fcefc4f435f0d4ed93a175d8
linnefromice <linnefromice@gmail.com>
2021-01-29 11:06:52 +0900

    update display (& model of evaluation including user / method for this)

--- lib/screens/evaluations_page.dart
+++ lib/screens/evaluations_page.dart
@@ -3,19 +3,48 @@ import 'package:flutter/material.dart';
 import 'package:linnefromice/components/wrapper_common_background.dart';
 import 'package:linnefromice/components/wrapper_fab_circle_menu.dart';
 import 'package:linnefromice/models/evaluation.dart';
+import 'package:linnefromice/models/user.dart';
 import 'package:linnefromice/services/evaluation_service.dart';
+import 'package:linnefromice/services/user_service.dart';
+
+// TODO: 暫定(本来はevaluationを拡張して、evaluationがUserを保持する形にしたい)
+class _EvaluationWithUser {
+  _EvaluationWithUser({
+    @required this.evaluation,
+    @required this.user,
+  });
+
+  final Evaluation evaluation;
+  final User user;
+}
 
 class EvaluationsPage extends StatelessWidget {
   final GlobalKey<FabCircularMenuState> fabKey = GlobalKey();
   final evaluationService = EvaluationService();
+  final userService = UserService();
+
+  // TODO: Simplize
+  Future<List<_EvaluationWithUser>> _findEvaluationWithUser() async {
+   final List<Evaluation> evaluations = await evaluationService.findEvaluations();
+   final List<User> users = await userService.findUsers();
+   final Map<String, User> userMap = Map.fromIterables(
+     users.map((e) => e.id).toList(),
+     users.map((e) => e).toList()
+   );
+   final List<_EvaluationWithUser> results = evaluations.map((e) => _EvaluationWithUser(
+     evaluation: e,
+     user: userMap[e.userId]
+   )).toList();
+   return results;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperCommonBackground(
-        child: FutureBuilder<List<Evaluation>> (
-          future: evaluationService.findEvaluations(),
-          builder: (BuildContext context, AsyncSnapshot<List<Evaluation>> snapshot) {
+        child: FutureBuilder<List<_EvaluationWithUser>> (
+          future: _findEvaluationWithUser(),
+          builder: (BuildContext context, AsyncSnapshot<List<_EvaluationWithUser>> snapshot) {
             if (snapshot.hasError) {
               return Center(child: Text(snapshot.error.toString()));
             }
@@ -23,13 +52,13 @@ class EvaluationsPage extends StatelessWidget {
               return ListView.builder(
                 itemCount: snapshot.data.length,
                 itemBuilder: (context, index) {
-                  final Evaluation item = snapshot.data[index];
+                  final _EvaluationWithUser item = snapshot.data[index];
                   return Card(
                     color: Colors.deepOrange[200].withOpacity(0.5),
                     child: ListTile(
-                      leading: Text(item.createdDate),
-                      title: Text("-> ${item.userId}"),
-                      trailing: Text(item.rating.toString()),
+                      leading: Text(item.evaluation.createdDate),
+                      title: Text("-> ${item.user.name}"),
+                      trailing: Text(item.evaluation.rating.toString()),
                     ),
                   );
                 },
