6c1ecaab79a9f3750d13886dd6e8d6d6d2837eb7
linnefromice <linnefromice@gmail.com>
2021-01-29 10:28:16 +0900

    add display evaluations (temp design)

--- lib/screens/evaluations_page.dart
+++ lib/screens/evaluations_page.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:linnefromice/components/wrapper_common_background.dart';
+import 'package:linnefromice/models/evaluation.dart';
+import 'package:linnefromice/services/evaluation_service.dart';
 
 class EvaluationsPage extends StatelessWidget {
+  final evaluationService = EvaluationService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperCommonBackground(
-        child: Center(child: Text("EvaluationPage")),
+        child: FutureBuilder<List<Evaluation>> (
+          future: evaluationService.findEvaluations(),
+          builder: (BuildContext context, AsyncSnapshot<List<Evaluation>> snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            }
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  final Evaluation item = snapshot.data[index];
+                  return Card(
+                    color: Colors.deepOrange[200].withOpacity(0.5),
+                    child: ListTile(
+                      leading: Text(item.createdDate),
+                      title: Text("-> ${item.userId}"),
+                      trailing: Text(item.rating.toString()),
+                    ),
+                  );
+                },
+              );
+            }
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
