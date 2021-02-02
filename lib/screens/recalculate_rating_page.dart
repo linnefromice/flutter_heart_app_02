5faa8d85df35171c40b4b3ab1cb45fb07ac84000
linnefromice <linnefromice@gmail.com>
2021-02-03 01:10:00 +0900

    create new page

---
+++ lib/screens/recalculate_rating_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:linnefromice/components/wrapper_common_background.dart';
+
+class RecalculateRatingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WrapperCommonBackground(
+        child: Center(
+          child: Text("RecalculateRatingPage"),
+        ),
+      ),
+    );
+  }
+}
