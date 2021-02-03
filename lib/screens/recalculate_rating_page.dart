2716590ff20148ba16cedf7c775d276851a99cd0
linnefromice <linnefromice@gmail.com>
2021-02-03 10:15:25 +0900

    add list (using dummy data)

--- lib/screens/recalculate_rating_page.dart
+++ lib/screens/recalculate_rating_page.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:linnefromice/components/wrapper_common_background.dart';
 
+const datas = [
+  {
+    "name": "Zidane Tribal",
+    "rating": 4,
+    "newRating": 3.5,
+  },
+  {
+    "name": "Dagger",
+    "rating": 4,
+    "newRating": 5
+  },
+  {
+    "name": "Vivi Orunitia",
+    "rating": 3,
+    "newRating": 4.25,
+  },
+  {
+    "name": "Adelbert Steiner",
+    "rating": 2,
+    "newRating": 2.75
+  },
+  {
+    "name": "Kuja",
+    "rating": 5,
+    "newRating": 5
+  },
+];
+
 class RecalculateRatingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperCommonBackground(
-        child: Center(
-          child: Text("RecalculateRatingPage"),
+        child: ListView.builder(
+          itemCount: datas.length,
+          itemBuilder: (context, index) {
+            final data = datas[index];
+            return Card(
+              child: ListTile(
+                title: Text(
+                  data["name"],
+                  style: TextStyle(fontSize: 12.0),
+                ),
+                subtitle: Text(
+                  "${data["rating"]} -> ${data["newRating"]}",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
