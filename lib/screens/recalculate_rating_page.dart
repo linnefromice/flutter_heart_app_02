236049d666723e60ddf9257d0e03afe6c8fbd0fb
linnefromice <linnefromice@gmail.com>
2021-02-03 10:30:32 +0900

    update list (use FutureBuilder)

--- lib/screens/recalculate_rating_page.dart
+++ lib/screens/recalculate_rating_page.dart
@@ -42,30 +42,51 @@ class _RatingInformation {
 }
 
 class RecalculateRatingPage extends StatelessWidget {
+  Future<List<_RatingInformation>> _getNewRatingList() async {
+    final List<_RatingInformation> results = datas.map((e) => _RatingInformation(
+      name: e["name"],
+      rating: e["rating"],
+      newRating: e["newRating"]
+    )).toList();
+    return results;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperCommonBackground(
-        child: ListView.builder(
-          itemCount: datas.length,
-          itemBuilder: (context, index) {
-            final data = datas[index];
-            return Card(
-              child: ListTile(
-                title: Text(
-                  data["name"],
-                  style: TextStyle(fontSize: 12.0),
-                ),
-                subtitle: Text(
-                  "${data["rating"]} -> ${data["newRating"]}",
-                  style: TextStyle(fontSize: 18.0),
-                ),
+        child: FutureBuilder<List<_RatingInformation>>(
+          builder: (BuildContext context, AsyncSnapshot<List<_RatingInformation>> snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            }
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  final _RatingInformation data = snapshot.data[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(data.name, style: TextStyle(fontSize: 12.0)),
+                      subtitle: Text("${data.rating} -> ${data.newRating}", style: TextStyle(fontSize: 18.0)),
+                    ),
+                  );
+                },
+              );
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  Text("Loading ...")
+                ],
               ),
             );
           },
         ),
-      ),
+      )
     );
   }
 }
