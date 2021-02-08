85d067ccc3526730986a426907bf38938597ec70
linnefromice <linnefromice@gmail.com>
2021-02-08 22:57:51 +0900

    create service

---
+++ lib/services/account_service.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:linnefromice/models/account.dart';
+
+class AccountService {
+  final _instance = FirebaseFirestore.instance;
+  final _collectionName = "accounts";
+  final _defaultSortKey = "updatedAt";
+
+  Account _generateModelFromQueryDocumentSnapshot(QueryDocumentSnapshot snapshot) => Account(
+    id: snapshot.id,
+    name: snapshot.data()["name"],
+    description: snapshot.data()["description"],
+    rating: snapshot.data()["rating"],
+    isFriend: snapshot.data()["isFriend"],
+    avatarUrl: snapshot.data()["avatarUrl"],
+    createdAt: snapshot.data()["createdAt"],
+    updatedAt: snapshot.data()["updatedAt"],
+    version: snapshot.data()["version"],
+  );
+
+  Stream<List<Account>> streamAccounts() {
+    return _instance.collection(_collectionName).orderBy(_defaultSortKey).snapshots().map(
+      (QuerySnapshot querySnapshot) => querySnapshot.docs.map(
+        (QueryDocumentSnapshot queryDocumentSnapshot) => _generateModelFromQueryDocumentSnapshot(queryDocumentSnapshot)
+      ).toList()
+    );
+  }
+
+  Account _generateModelFromDocumentSnapshot(DocumentSnapshot snapshot) => Account(
+    id: snapshot.id,
+    name: snapshot.data()["name"],
+    description: snapshot.data()["description"],
+    rating: snapshot.data()["rating"],
+    isFriend: snapshot.data()["isFriend"],
+    avatarUrl: snapshot.data()["avatarUrl"],
+    createdAt: snapshot.data()["createdAt"],
+    updatedAt: snapshot.data()["updatedAt"],
+    version: snapshot.data()["version"],
+  );
+
+  Future<List<Account>> findAccounts() async {
+    QuerySnapshot querySnapshot = await _instance.collection(_collectionName).orderBy(_defaultSortKey).get();
+    return querySnapshot.docs.map(
+      (DocumentSnapshot documentSnapshot) => _generateModelFromDocumentSnapshot(documentSnapshot)
+    ).toList();
+  }
+  
+  Future<void> createAccount({ final String name, final String description, final double rating, final bool isFriend, final String avatarUrl }) async {
+    final DateTime now = DateTime.now();
+    _instance.collection(_collectionName).add(
+        Account(
+        name: name,
+        description: description,
+        rating: rating,
+        isFriend: isFriend,
+        avatarUrl: avatarUrl,
+        createdAt: now.toIso8601String(),
+        updatedAt: now.toIso8601String(),
+        version: 1
+      ).toJson()
+    );
+  }
+
+  Future<void> deleteAccount(final String id) async {
+    _instance.collection(_collectionName).doc(id).delete();
+  }
+
+  Future<void> updateRating(final String id, final int currentVersion, final double newRating) async {
+    final DateTime now = DateTime.now();
+    _instance.collection(_collectionName).doc(id).update({
+      "rating": newRating,
+      "updatedAt": now.toIso8601String(),
+      "version": currentVersion + 1
+    });
+  }
+}
