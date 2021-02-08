0ba941a1a81162ec24b86bae5e280b8a60d58972
linnefromice <linnefromice@gmail.com>
2021-02-08 23:49:28 +0900

    minor collection

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -3,16 +3,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthenticationService {
   final _instance = FirebaseAuth.instance;
 
-  Future<bool> authenticate({final String email, final String domain, final String password}) async {
+  Future<String> authenticate({final String email, final String domain, final String password}) async {
     try {
       await _instance
           .signInWithEmailAndPassword(
           email: email + "@" + domain,
           password: password
       );
-      return true;
+      return null;
     } on FirebaseAuthException catch (e) {
-      return false;
+      if (e.code == 'user-not-found') {
+        return "No user found for that email.";
+      } else if (e.code == 'wrong-password') {
+        return "Wrong password provided for that user.";
+      }
+      return "Internal Server Error";
     }
   }
 }
