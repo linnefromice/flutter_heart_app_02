cf671b1a9f8b7e046fbe6e295ba89f9a6b23e0e4
linnefromice <linnefromice@gmail.com>
2021-02-10 00:59:03 +0900

    update authentication_service - save user/account information

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -1,15 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:linnefromice/models/account.dart';
+import 'package:linnefromice/services/account_service.dart';
+
+final FirebaseAuth _firebaseAuthInstance = FirebaseAuth.instance;
+
 
 class AuthenticationService {
-  final _instance = FirebaseAuth.instance;
+  final accountService = AccountService();
+  static Account currentAccount;
+
+  User get currentUser => _firebaseAuthInstance.currentUser;
 
   Future<String> authenticate({final String email, final String domain, final String password}) async {
     try {
-      await _instance
+      UserCredential credential = await _firebaseAuthInstance
         .signInWithEmailAndPassword(
           email: email + "@" + domain,
           password: password
         );
+      currentAccount = await accountService.findAccount(credential.user.uid);
       return null;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
