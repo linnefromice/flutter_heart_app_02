b5ec29f5259ced6cca171c8ecef27d9dbae374dc
linnefromice <linnefromice@gmail.com>
2021-02-10 10:09:59 +0900

    create new page

---
+++ lib/screens/settings_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:linnefromice/components/wrapper_common_background.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WrapperCommonBackground(
+        child: Center(child: Text("SettingsPage")),
+      ),
+    );
+  }
+}
