06aa9d2d0384050e730c98a1b01f0c569b4a4a49
linnefromice <linnefromice@gmail.com>
2021-02-13 11:34:02 +0900

    update authentication service - add signup method

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -29,4 +29,26 @@ class AuthenticationService {
       return "Internal Server Error";
     }
   }
+
+  Future<String> signUp({final String email, final String password}) async {
+    try {
+      UserCredential credential = await _firebaseAuthInstance
+        .createUserWithEmailAndPassword(
+          email: email,
+          password: password
+        );
+      currentAccount = await accountService.findAccount(credential.user.uid); // TODO エラーハンドリング
+      return null;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return "The password provided is too weak.";
+      } else if (e.code == 'email-already-in-use') {
+        return "The account already exists for that email.";
+      }
+      return "Internal Server Error";
+    } catch (e) {
+      print(e);
+      return "Internal Server Error";
+    }
+  }
 }
